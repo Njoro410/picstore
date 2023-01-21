47bcf8bc349316b8d307745c5a0f90af4e1ee820
njoro410 <brian.njoroge@student.moringaschool.com>
2023-01-21 16:55:59 +0300

    added view function to get all trucks

--- fjc/views.py
+++ fjc/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from fjc.models import User,Truck,Accessories,Collection,CollectionImage,Color
-from .serializers import ColorSerializer
+from .serializers import ColorSerializer,TruckSerliazer
 
 # Create your views here.
 @api_view(['GET'])
@@ -10,3 +10,10 @@ def GetColors(request):
     colors = Color.objects.all()
     serializer = ColorSerializer(colors, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def GetTruck(request):
+    trucks = Truck.objects.all()
+    serializer = TruckSerliazer(trucks, many=True)
+    return Response(serializer.data)
+    
