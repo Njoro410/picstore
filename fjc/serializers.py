bab5f5ce138025507ddd380f7383c28283226db7
njoro410 <brian.njoroge@student.moringaschool.com>
2023-01-21 16:56:26 +0300

    Feature: added a function to serialize all data passed from view function

--- fjc/serializers.py
+++ fjc/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Color
+from .models import Color,Truck
 
 
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Color
+        fields = '__all__'
+        
+
+class TruckSerliazer(serializers.ModelSerializer):
+    class Meta:
+        model = Truck
         fields = '__all__'
