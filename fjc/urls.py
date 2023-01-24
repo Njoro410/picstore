6686e8315591d9916d84f4837f81b62469a23856
njoro410 <brian.njoroge@student.moringaschool.com>
2023-01-24 09:23:41 +0300

    URLs: added urls to get different endpoints

--- fjc/urls.py
+++ fjc/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('colors/', views.GetColors, name='allcolors')
+    path('colors/', views.GetColors, name='allcolors'),
+    path('trucks/', views.GetTruck, name='alltruck'),
+    path('accessories/', views.GetAccessories, name='allaccessories'),
+    path('collections/', views.GetCollections, name='allcollections'),
+    path('collectionimages/<str:id>/', views.GetCollectionImages, name='collectionimages')
 ]
