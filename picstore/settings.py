dd62a16076dc3eb9f37811e3a0932f0d1690dae8
njoro410 <brian.njoroge@student.moringaschool.com>
2023-03-07 03:33:36 +0300

    changed static directory

--- picstore/settings.py
+++ picstore/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(kzx6cp8fc&@he6(p_$ies)yxf-tmp7a!ml5#2j()nk&*y7x0s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
